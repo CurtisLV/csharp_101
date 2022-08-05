ac6446e2f22b2f6d6cea4e9ab54ef35f325ea120
CurtisLV <curtislv@gmail.com>
2022-08-05 17:14:50 +0300

    17:14

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,21 @@
+﻿// Namespace
+namespace NumberGuesser
+    {
+    // Main Class
+    internal class Program
+        {
+        // Entry Point method
+        static void Main(string[] args)
+            {
+            // Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Kārlis M.";
+
+            // Change console text color
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            }
+        }
+    }
