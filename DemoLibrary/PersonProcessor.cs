39fe399880365dc7e2d580c7016730a13d2bf93f
CurtisLV <curtislv@gmail.com>
2022-08-06 12:43:39 +0300

    12:43

---
+++ DemoLibrary/PersonProcessor.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DemoLibrary;
+
+public class PersonProcessor
+{
+    public string JoinName(string firstName, string lastName)
+    {
+        return firstName + " " + lastName;  
+    }
+}
