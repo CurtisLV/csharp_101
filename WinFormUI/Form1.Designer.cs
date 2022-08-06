eea3e4b68956246e052e046985281095191bca57
CurtisLV <curtislv@gmail.com>
2022-08-06 13:52:15 +0300

    13:52

---
+++ WinFormUI/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.combineName = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(70, 47);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(199, 50);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            this.firstNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(275, 47);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(214, 57);
+            this.firstNameText.TabIndex = 1;
+            this.firstNameText.Click += new System.EventHandler(this.firstNameText_Click);
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(275, 152);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(214, 57);
+            this.lastNameText.TabIndex = 3;
+            this.lastNameText.TextChanged += new System.EventHandler(this.lastNameText_Click);
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(70, 152);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(195, 50);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            this.lastNameLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // combineName
+            // 
+            this.combineName.Location = new System.Drawing.Point(109, 258);
+            this.combineName.Name = "combineName";
+            this.combineName.Size = new System.Drawing.Size(380, 90);
+            this.combineName.TabIndex = 4;
+            this.combineName.Text = "Combine name";
+            this.combineName.UseVisualStyleBackColor = true;
+            this.combineName.Click += new System.EventHandler(this.combineName_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 50F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 416);
+            this.Controls.Add(this.combineName);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label firstNameLabel;
+        private TextBox firstNameText;
+        private TextBox lastNameText;
+        private Label lastNameLabel;
+        private Button combineName;
+    }
+}
---
+++ WinFormUI/Form1.cs
@@ -0,0 +1,35 @@
+namespace WinFormUI;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click_1(object sender, EventArgs e)
+    {
+
+    }
+    private void firstNameText_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void lastNameText_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void combineName_Click(object sender, EventArgs e)
+    {
+
+    }
+
+
+}
