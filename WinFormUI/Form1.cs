c341469d0fbb719f02ccae6ab976de33587118b0
CurtisLV <curtislv@gmail.com>
2022-08-06 14:01:17 +0300

    14:01

--- WinFormUI/Form1.cs
+++ WinFormUI/Form1.cs
@@ -1,3 +1,5 @@
+using DemoLibrary;
+
 namespace WinFormUI;
 
 public partial class Form1 : Form
@@ -11,7 +13,6 @@ public partial class Form1 : Form
     {
 
     }
-
     private void label1_Click_1(object sender, EventArgs e)
     {
 
@@ -28,7 +29,9 @@ public partial class Form1 : Form
 
     private void combineName_Click(object sender, EventArgs e)
     {
-
+        string fullName = PersonProcessor.JoinName(firstNameText.Text, lastNameText.Text);
+        string msg = $"Your full name is {fullName}";
+        MessageBox.Show(msg);
     }
 
 
